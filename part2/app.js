e5d1d22211a0ded00248ecf94483927b842a6b50
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-20 09:02:49 +0000

    [automatic commit] 2025-06-20 09:02:49 UTC

--- part2/app.js
+++ part2/app.js
@@ -7,8 +7,6 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
 app.use(
     session({
         secret: process.env.SESSION_SECRET || 'my_secret123',
@@ -17,6 +15,8 @@ app.use(
         cookie: { secure: false }
     })
 );
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
