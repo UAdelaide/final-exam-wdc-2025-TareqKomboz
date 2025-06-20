28566b19cc6804b62a42792d2fdba2827f1897df
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-20 06:41:40 +0000

    [automatic commit] 2025-06-20 06:41:40 UTC

--- part1/app.js
+++ part1/app.js
@@ -60,7 +60,18 @@ app.get("/api/dogs", async (req, res) => {
 // Route to get all open walk requests with dog and owner username
 app.get("/api/walkrequests/open", async (req, res) => {
     try {
-        const [rows] = await pool.query();
+        const [rows] = await pool.query(
+            `SELECT w.request_id,
+                d.name   AS dog_name,
+                w.requested_time,
+                w.duration_minutes,
+                w.location,
+                u.username AS owner_username
+        FROM WalkRequests w
+        JOIN Dogs d   ON w.dog_id = d.dog_id
+        JOIN Users u  ON d.owner_id = u.user_id
+        WHERE w.status = 'open'`
+        );
       } catch (err) {
         res.status(500).json({ error: "Failed to fetch walk requests." });
       }
