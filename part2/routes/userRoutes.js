6c5a3536ab0783579ff8c873748f9b5843a75b34
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-20 09:38:16 +0000

    [automatic commit] 2025-06-20 09:38:16 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -39,8 +39,8 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
-  // const { username, password_hash } = req.body;
-  // console.log(username);
+  const { username, password_hash } = req.body;
+  console.log(username);
   // console.log(password_hash);
 
   try {
