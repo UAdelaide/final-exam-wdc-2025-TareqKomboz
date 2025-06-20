9c50f698062a8087cf3a868aedf6e4b4fe021dc4
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-20 08:43:38 +0000

    [automatic commit] 2025-06-20 08:43:38 UTC

--- part2/app.js
+++ part2/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(
     session({
-        secret: process.env.SESSION_SECRET || 'replace-me',
+        secret: process.env.SESSION_SECRET || '',
         resave: false,
         saveUninitialized : false,
         cookie: { secure: false } // set true if HTTPS
