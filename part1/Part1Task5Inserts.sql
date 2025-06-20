126172bd1d3152b16179b44b7cf9fb549ca93273
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-20 06:32:24 +0000

    [automatic commit] 2025-06-20 06:32:24 UTC

--- part1/Part1Task5Inserts.sql
+++ part1/Part1Task5Inserts.sql
@@ -1,6 +1,6 @@
 INSERT INTO Users (username, email, password_hash, role)
 VALUES
-  ('alice123',  'alice@example.com',  'hashed123', 'owner'),
+('alice123',  'alice@example.com',  'hashed123', 'owner'),
   ('bobwalker', 'bob@example.com',   'hashed456', 'walker'),
   ('carol123',  'carol@example.com', 'hashed789', 'owner'),
   ('tareq',     'tareq@example.com',  'hashed000', 'walker'),
