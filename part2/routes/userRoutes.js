bd80f8aaaf3c46816d979fb27a329a5ae232b3bd
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-20 09:58:59 +0000

    [automatic commit] 2025-06-20 09:58:58 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -71,8 +71,6 @@ router.post('/logout', (req, res) => {
     res.clearCookie('connect.sid');
     res.json({ message: 'Logged out' });
   });
-
-  window.location.href = 'index.html';
 });
 
 module.exports = router;
