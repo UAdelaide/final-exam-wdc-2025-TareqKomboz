6666094065a340441b531cff5ada2d851e3c18b1
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-21 04:24:25 +0000

    [automatic commit] 2025-06-21 04:24:25 UTC

--- part2/app.js
+++ part2/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogRouter = require('./routes/dogs');
+const dogRouter = require('./routes/dogRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
