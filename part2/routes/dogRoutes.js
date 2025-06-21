a3ab693f2bf0d12aba4f755523883800c37119bc
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-21 05:29:13 +0000

    [automatic commit] 2025-06-21 05:29:13 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -21,9 +21,7 @@ router.get('/mine', async (req, res) => {
 router.get("/", async (req, res) => {
   try {
       const [rows] = await db.query(
-          `SELECT d.name AS dog_name,
-                  d.size,
-                  u.username AS owner_username
+          `SELECT *
           FROM Dogs d
           JOIN Users u ON d.owner_id = u.user_id`
       );
