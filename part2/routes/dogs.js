2718bfecce7adb60c2f246b0a29f8e30177cf38b
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-21 04:15:36 +0000

    [automatic commit] 2025-06-21 04:15:36 UTC

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../models/db');
 const auth = require('../middleware/auth'); // session check
 
