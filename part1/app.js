25f4615a07a0cd14f34fd8b8481103803313ef4c
TareqKomboz <Tareq.Komboz@hotmail.com>
2025-06-21 05:50:20 +0000

    [automatic commit] 2025-06-21 05:50:20 UTC

--- part1/app.js
+++ part1/app.js
@@ -39,7 +39,22 @@ let db;
   }
 })();
 
-// Route to list every dog with size and owner username
+Return a list of all dogs with their size and owner's username.
+
+Sample Response:
+
+[
+  {
+    "dog_name": "Max",
+    "size": "medium",
+    "owner_username": "alice123"
+  },
+  {
+    "dog_name": "Bella",
+    "size": "small",
+    "owner_username": "carol123"
+  }
+]
 app.get("/api/dogs", async (req, res) => {
     try {
         const [rows] = await db.query(
